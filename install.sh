08f8e07226397d7750591398528403739e0250b4
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-07-14 21:59:14 +0300

    Add git configuration to install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ DOTFILES="$HOME/dotfiles"
 ln -sf "$DOTFILES/.zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES/.zshrc" "$HOME/.zshrc"
 
+# .zshenv sources .secrets
+source "$HOME/.zshenv"
+
 # Install Xcode Command Line Tools
 xcode-select --install
 
@@ -15,6 +18,19 @@ xcode-select --install
 # Symlink Brewfile and install packages
 ln -sf "$DOTFILES/Brewfile" "$HOME/Brewfile"
 brew bundle
+
+# Set up Git
+git config --global user.name "$GIT_AUTHOR_NAME"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
+git config --global init.defaultBranch "main"
+git config --global core.editor "$VISUAL_EDITOR"
+git config --global core.excludesfile "$HOME/.gitignore_global"
+# Symlink .gitignore_global
+ln -sf "$DOTFILES/.gitignore_global" "$HOME/.gitignore_global"
+# Authenticate GitHub CLI
+gh auth login
+# Configure git to use GitHub CLI as the credential helper
+gh auth setup-git
 
 # Install Volta and Node
 curl https://get.volta.sh | bash
