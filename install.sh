333a42492cb23cf2cd22a1c23c86d89b1631e740
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-07-14 21:58:29 +0300

    Refactor install.sh and .zshenv

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/zsh
 
+DOTFILES="$HOME/dotfiles"
+
 # Symlink Zsh startup files
-ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
-ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -sf "$DOTFILES/.zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/.zshrc" "$HOME/.zshrc"
 
 # Install Xcode Command Line Tools
 xcode-select --install
 
-# Install Homebrew if not installed
+# Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Symlink Brewfile and install packages
-ln -sf $HOME/dotfiles/Brewfile $HOME/Brewfile
+ln -sf "$DOTFILES/Brewfile" "$HOME/Brewfile"
 brew bundle
 
 # Install Volta and Node
