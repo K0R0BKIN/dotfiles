d32d01316c4428889056016589d16d128486743d
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-02-09 02:14:18 +0300

    Add `gh auth login` to install.sh
    Needed for Git auth.

--- install.sh
+++ install.sh
@@ -40,3 +40,4 @@ volta install node
 git config --global user.name "Nikita Korobkin"
 git config --global user.email "nikita.korobkin.personal@gmail.com"
 git config --global init.defaultBranch "main"
+gh auth login
