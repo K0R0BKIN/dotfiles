7ac2fd76b3500070367d42bb9a100fbe15b35488
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-02-10 20:09:13 +0300

    Format install.sh

--- install.sh
+++ install.sh
@@ -2,32 +2,26 @@
 set -e
 
 DOTFILES="$HOME/dotfiles"
-ICLOUD="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
-
-# Homebrew (before stow so we can install it)
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
-brew install stow
 
-# Symlinks
-
-ln -sf "$ICLOUD/secrets/.secrets" "$HOME/.secrets"
-cd "$DOTFILES"
-stow brew ghostty git zed zsh
-
-brew bundle
 brew tap domt4/autoupdate
 brew autoupdate start --upgrade
 
-# Volta
+ICLOUD="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
+ln -sf "$ICLOUD/secrets/.secrets" "$HOME/.secrets"
+
+brew install stow
+cd "$DOTFILES" && stow brew ghostty git zed zsh
+
+brew bundle
 
 curl https://get.volta.sh | bash
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
 volta completions zsh > "$HOME/.volta-completions.zsh"
-volta install node
 
-# Git
+volta install node
 
 gh auth status &>/dev/null || gh auth login
