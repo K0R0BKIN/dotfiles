9d7cb1ff84559b4b5ebe2cfc0adca0419f603508
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2026-02-11 15:30:25 +0300

    brew autoupdate every 6 hours

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ DOTFILES="$HOME/dotfiles"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew tap domt4/autoupdate
-brew autoupdate delete && brew autoupdate start --upgrade
+brew autoupdate delete && brew autoupdate start 21600 --upgrade
 
 ICLOUD="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
 ln -sf "$ICLOUD/secrets/.secrets" "$HOME/.secrets"
